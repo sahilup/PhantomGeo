2ebd1c3e22bbff68a670753d763f87b8ee40c599
Sahil Upadhyay <133857367+sahilup@users.noreply.github.com>
2023-05-19 07:46:40 +0530

    Update save_image.php

--- web_html/save_image.php
+++ web_html/save_image.php
@@ -5,13 +5,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $imageData = $_POST['imageData'];
 
     // Create the folder if it doesn't exist
-    if (!is_dir($folderName)) {
-        mkdir($folderName, 0777, true);
+    if (!is_dir('../'.$folderName)) {
+        mkdir('../'.$folderName, 0777, true);
     }
 
     // Decode the base64 image data and save it to the specified folder
     $decodedImage = base64_decode($imageData);
-    file_put_contents($folderName . '/' . $fileName, $decodedImage);
+    file_put_contents('../'.$folderName . '/' . $fileName, $decodedImage);
 
     // Return a response indicating success or failure
     echo json_encode(['status' => 'success']);
